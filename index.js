da9170decf71faccbe15173f9fb4070de96ebc94
Mark-A-A <markawai01@gmail.com>
2016-02-18 17:11:03 -0500

    handlebars test render

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
  
 var app = express();
+// Directory for serving public static files
+app.use('/static', express.static(__dirname + '/public'));
 
 //Handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
  
 app.get('/', function (req, res) {
+    console.log("hit the home page");
     res.render('home');
 });
  
