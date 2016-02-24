9a5d365d8520fe5278b1bd8a5e6c3b5d3f4b1f09
Mark-A-A <markawai01@gmail.com>
2016-02-23 19:45:15 -0500

    debugging deploy to heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-debugger
+// debugger
 var PORT = process.env.NODE_ENV || 4040;
 
 var express = require('express');
@@ -25,10 +25,10 @@ app.use(express.static('public'));
 var connection = mysql.createConnection({
   
   port     : 3306,
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'burgers_db'
+  host     : '3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'yrt6xjrsqcx7fth1',
+  password : 'yk6nduh7mluc88wk',
+  database : 'qaxwxfia7ft9ok98'
 });
  
 connection.connect();
@@ -44,7 +44,7 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 //routes go inside controller JS file
 
 app.get('/', function (req, res) {
-    debugger
+    // debugger
     console.log("hit the home page");
     var query = ("SELECT * FROM burgers");
     connection.query(query, function (err, results) {
@@ -62,7 +62,7 @@ app.get('/', function (req, res) {
 
 
 app.post("/create", function (req, res){
-    debugger
+    // debugger
     console.log("Posting to burgers table in burgers_db");
     console.log(req);
     console.log("request body: "+ req.body)
